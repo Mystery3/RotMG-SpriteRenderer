30cfb053a8cc1ed9668db94e68a58e05b49ae880
Mystery3 <maksilok@gmail.com>
2024-03-21 15:09:56 -0400

    tweaked build to delete readme and license

--- build.bat
+++ build.bat
@@ -8,15 +8,22 @@ pyinstaller ^
     --icon ./bin/favicon.ico ^
     Renderer.py 
 
+rem pyinstaller cleanup
 move "%cd%\dist\Renderer.exe" "%cd%"
 rd "%cd%\dist"
 rd /s /q "%cd%\build"
-del "%cd%\.gitignore"
 del "%cd%\Renderer.py"
 del "%cd%\Renderer.spec"
 
+rem git cleanup
+del "%cd%\.gitignore"
+del "%cd%\LICENSE"
+del "%cd%\README.md"
+
+rem default folders
 md "%cd%\Renders"
 md "%cd%\Textiles"
 md "%cd%\Sheets"
 
+rem self cleanup
 del "%cd%\build.bat"
